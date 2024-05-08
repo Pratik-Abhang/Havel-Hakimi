b8301a08ba16d5ac0b1e36750f784666ed981382
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-05-08 23:02:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit as st 
 st.title("Havel Hakimi Algorithm App")
-st.text("This app will help you to check whether the degree sequence is graphical or not , if yes it will show you the graph of it")
+st.text("This app will help you to check whether the degree sequence is graphical or not")
+st.text("if yes it will show you the graph of it")
