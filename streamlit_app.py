322383f94f433767c34e52c983988e0dc6d28313
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-06-20 19:15:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit as st
 st.title("Havel Hakimi Algorithm App")
 st.text("This app will help you to check whether the degree sequence is graphical or not")
 st.text("if yes it will show you the graph of it")
-st.number_input("enter degree sequence seperated by comma")
+user_input = st.text_input("enter degree sequence seperated by comma")
+
+for i in user_input:
+  print(i)
 
