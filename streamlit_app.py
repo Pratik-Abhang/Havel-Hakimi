d629885ecda5ba9eb9254e236faacc1c6a42ce4e
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-06-26 12:24:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,31 @@
 import streamlit as st 
-st.title("Havel Hakimi Algorithm App")
-st.text("This app will help you to check whether the degree sequence is graphical or not")
-st.text("if yes it will show you the graph of it")
-user_input = st.text_input("enter degree sequence seperated by comma")
-user_str = user_input.replace(',', '')
-user_str = user_str.split()
-deg_seq = []
-for i in user_str:
-    deg_seq.append(user_str)
-
 import networkx as nx
 import matplotlib.pyplot as plt
 
+st.title("Havel Hakimi Algorithm App")
+st.text("This app will help you to check whether the degree sequence is graphical or not")
+st.text("if yes it will show you the graph of it")
+
+user_input = st.text_input("enter degree sequence separated by comma")
+user_str = user_input.replace(',', ' ')
+user_list = user_str.split()
+deg_seq = [int(x) for x in user_list]
+
 def HHA(deg_seq):
     if not deg_seq:
-        print("Sequence is empty")
+        st.write("Sequence is empty")
         return False
     deg_seq = sorted(deg_seq, reverse=True)
     s = sum(deg_seq)
     if s % 2 != 0:
-        print("Given sequence is not graphical")
+        st.write("Given sequence is not graphical")
         return False
     while True:        
         if deg_seq[0] < 0 or deg_seq[0] >= len(deg_seq):
-            print("Given sequence is not graphical")
+            st.write("Given sequence is not graphical")
             return False
         if all(d == 0 for d in deg_seq):
-            print("Given sequence is graphical")
-            #print(deg_seq)
+            st.write("Given sequence is graphical")
             return True
         for i in range(1, deg_seq[0] + 1):
             deg_seq[i] -= 1
@@ -38,10 +36,7 @@ def plot_graph(deg_seq):
     if HHA(deg_seq):
         G = nx.random_degree_sequence_graph(deg_seq)
         nx.draw(G, with_labels=True, node_color='purple', node_size=500, font_size=10)
-        plt.show()
+        st.pyplot(plt)
 
 if st.button("Apply"):
-  plot_graph(deg_seq)
-
-
-
+    plot_graph(deg_seq)
